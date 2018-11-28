24860a169d804f27460cf90c010c1e345ec676a1
puszynski <33966744+puszynski@users.noreply.github.com>
2018-11-28 06:40:52 +0100

    Refactor - replace devideMethod into the controller, add js for typing view page

--- typingMVCApp/Controllers/TypingController.cs
+++ typingMVCApp/Controllers/TypingController.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
+using TypingMVCApp.DAL;
+using TypingMVCApp.Helper;
+using TypingMVCApp.ViewModels;
 
 namespace TypingMVCApp.Controllers
 {
     public class TypingController : Controller
     {
+        private EntityFrameworkDBContext db = new EntityFrameworkDBContext();
+
         // GET: Typing
         public ActionResult Index(int bookID = 1, int bookPage = 1)
         {
-            return View();
+            var book = db.Books.Find(bookID);
+            //var authors = db.Authors.Find(); ??
+            var typingHelper = new TypingHelper();
+
+            var result = new TypingViewModel()
+            {
+                BookAuthors = new List<string> { "ToDO1", "ToDO2" },
+                CurrentBookPage = bookPage,
+                BookPages = typingHelper.DivideBook(book.BookContent),
+                BookTitle = book.BookTitle,
+                BookID = bookID
+            };
+
+            return View(result);
         }
     }
 }
---
+++ typingMVCApp/ViewModels/TypingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using TypingMVCApp.Helper;
+
+namespace TypingMVCApp.ViewModels
+{
+    public class TypingViewModel
+    {
+        public int BookID { get; set; }
+        public List<string> BookPages { get; set; }
+        public string BookTitle { get; set; }
+        public int CurrentBookPage { get; set; }
+        public List<string> BookAuthors { get; set; }        
+    }
+}
