3ca1fcefd9208fd329cca6fbe1f6ce5df54add17
puszynski <33966744+puszynski@users.noreply.github.com>
2018-12-08 10:39:33 +0100

    AddCoreProjectAndBookAuthorModelsAndBooksController

---
+++ TypingMVCCore/DomainModels/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TypingMVCCore.DomainModels
+{
+    public class Book
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string BookTitle { get; set; }
+
+        [Required]
+        public string BookContent { get; set; }
+
+        //public virtual ICollection<Author> Authors { get; set; }
+
+        //public virtual ICollection<Genre> GenresEnum { get; set; }
+
+        [Range(1, 10)]
+        public int? Rate { get; set; }
+    }
+
+
+    public enum Genre
+    {
+        NoGenre = 0,
+        SciFi = 1,
+        Horror = 2,
+        Action = 3,
+        Travel = 4
+    }
+}
