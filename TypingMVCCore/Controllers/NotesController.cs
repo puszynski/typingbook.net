87efc78b6f5b77cc222038194ce4cedbac8268e7
Jakub Puszyński <jakub.puszynski@efitness.com>
2018-12-21 12:59:04 +0100

    Note controller - add domain test, notesController and some applicartionDbContext

---
+++ TypingMVCCore/Controllers/NotesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TypingMVCCore.Data;
+using TypingMVCCore.DomainModels;
+
+namespace TypingMVCCore.Controllers
+{
+    /// <summary>
+    /// Store private users notes
+    /// </summary>
+    public class NotesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public NotesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<IActionResult> Index(string searchString)
+        {
+            var notes = _context.Note.Select(x => x);
+
+            if (!String.IsNullOrEmpty(searchString))
+                notes = notes.Where(x => x.NoteTitle.Contains(searchString));
+
+            return View(await notes.ToListAsync());
+        }
+
+        // GET: Books/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Notes/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,NoteTitle,NoteContent,UserID")] Note note)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(note);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(note);
+        }
+
+        // GET: Books/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var note = await _context.Note.FindAsync(id);
+
+            if (note == null)
+                return NotFound();
+
+            return View(note);
+        }
+
+        // POST: Books/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,NoteTitle,NoteContent,UserID")] Note note)
+        {
+            if (id != note.ID)
+                return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(note);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NoteExists(note.ID))
+                        return NotFound();
+                    else
+                        throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(note);
+        }
+
+        // GET: Books/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var note = await _context.Note
+                .FirstOrDefaultAsync(m => m.ID == id);
+
+            if (note == null)
+                return NotFound();
+
+            return View(note);
+        }
+
+        // POST: Books/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var note = await _context.Note.FindAsync(id);
+            _context.Note.Remove(note);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NoteExists(int id)
+        {
+            return _context.Book.Any(e => e.ID == id);
+        }
+    }
+}
+}
---
+++ TypingMVCCore/DomainModels/Note.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TypingMVCCore.DomainModels
+{
+    public class Note
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Note Title")]
+        public string NoteTitle { get; set; }
+
+        [Required]
+        public string NoteContent { get; set; }
+
+
+        // ToDo - relacja one to one
+        //public virtual User User { get; set; }
+
+        //vs.
+
+        //public int UserID { get; set; }
+        //public User User { get; set; }
+    }
+}
